17c72a09013162309f4f542fcb758f4786c33dc1
Adrien Thebo <git@somethingsinistral.net>
2016-11-18 10:39:45 -0800

    (maint) Improve puppet face documentation

--- lib/puppet/face/certregen.rb
+++ lib/puppet/face/certregen.rb
@@ -3,10 +3,23 @@ require 'puppet_x/certregen/ca'
 require 'puppet_x/certregen/certificate'
 
 Puppet::Face.define(:certregen, '0.1.0') do
+  copyright "Puppet", 2016
   summary "Regenerate the Puppet CA and client certificates"
+
+  description <<-EOT
+    This subcommand provides tools for monitoring the health of the Puppet CA, regenerating
+    expiring CA certificates, and remediation for expired CA certificates.
+  EOT
 
   action(:ca) do
     summary "Regenerate the Puppet CA certificate"
+
+    description <<-EOT
+      This subcommand generates a new CA certificate that can replace the existing CA certificate.
+      The new CA certificate uses the same subject as the current CA certificate and reuses the
+      key pair associated with the current CA certificate, so all certificates signed by the old
+      CA certificate will remain valid.
+    EOT
 
     option('--ca_serial SERIAL') do
       summary 'The serial number (in hexadecimal) of the CA to rotate.'
@@ -36,8 +49,12 @@ Puppet::Face.define(:certregen, '0.1.0') do
   action(:healthcheck) do
     summary "Check for expiring certificates"
 
+    description <<-EOT
+      This subcommand checks for certificates that are nearing or past expiration.
+    EOT
+
     option('--all') do
-      summary 'Report certificate expiry for all nodes'
+      summary "Report certificate expiry for all nodes, including nodes that aren't near expiration."
     end
 
     when_invoked do |opts|
