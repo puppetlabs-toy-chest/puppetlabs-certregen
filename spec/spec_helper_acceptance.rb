813b8939e62eef8add94081e5ae9c3593e3b2d5e
John Duarte <john.duarte@puppetlabs.com>
2017-03-01 15:00:10 -0800

    (MODULES-4315) Add acceptance test for help
    This commit adds an initial acceptance test to validate that
    appropriate help text is presented when the help subcommand and
    flag are used.

---
+++ spec/spec_helper_acceptance.rb
@@ -0,0 +1,14 @@
+require 'beaker-rspec'
+require 'beaker/puppet_install_helper'
+require 'beaker/module_install_helper'
+
+run_puppet_install_helper
+install_ca_certs unless ENV['PUPPET_INSTALL_TYPE'] =~ /pe/i
+install_module_on(hosts)
+install_module_dependencies_on(hosts)
+
+RSpec.configure do |c|
+  # Readable test descriptions
+  c.formatter = :documentation
+end
+
