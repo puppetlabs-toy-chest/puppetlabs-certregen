48ba33a140c7ccf60cbf71e01c1df47e423dc392
John Duarte <john.duarte@puppetlabs.com>
2017-04-05 08:47:56 -0700

    (MODULES-4589) Iterate on install_module_on
    This commit calls `install_module_on` for individual host objects
    rather than an array of host objects. There is currently a bug in
    this helper that uses the `distmoduledir` value for the first host
    object in an array and forces it upon the remaining host objects.

--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -5,7 +5,9 @@ require 'acceptance/helpers'
 
 run_puppet_install_helper
 install_ca_certs unless ENV['PUPPET_INSTALL_TYPE'] =~ /pe/i
-install_module_on(hosts)
+hosts.each do |host|
+  install_module_on(host)
+end
 install_module_dependencies_on(hosts)
 
 RSpec.configure do |c|
