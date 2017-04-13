2ac743227b92b664e65f872bf34aba44d4291703
John Duarte <john.duarte@puppetlabs.com>
2017-04-13 14:00:05 -0700

    (MODULES-4726) Only test on ruby 1.9 or above
    This commit adds conditional logic for the `puppet help certregen`
    to ensure that the ruby version is greater than 1.8.x. These tests
    run on all agents. Currently, certregen is not compatible with ruby
    versions less than 1.9.

--- spec/acceptance/help_spec.rb
+++ spec/acceptance/help_spec.rb
@@ -1,34 +1,39 @@
 require 'spec_helper_acceptance'
 
-describe "C98923 - Verify that 'puppet certregen --help' prints help text" do
-  # NOTE: `--help` only works on puppet version 4+
-  major = 0
-  on(default, puppet('--version')) do |result|
-    x,y = result.stdout.split('.')
-    major = x.to_i
+describe "pupper help certregen" do
+  # NOTE: MODULES-4733 certregen is not currently compatible with ruby < 1.9
+  ruby_ver = 0
+  on(default, 'ruby --version') do |result|
+    m = /\d+\.\d+\.\d+/.match(result.stdout)
+    ruby_ver = m[0] if m
   end
-  if major > 3 then
-    describe command("puppet certregen --help") do
-      its(:stdout) { should match( /.*USAGE: puppet certregen <action>.*/ ) }
-      its(:stdout) { should match( /.*See 'puppet man certregen' or 'man puppet-certregen' for full help.*/ ) }
+  unless version_is_less(ruby_ver, '1.9') then
+    describe "C98923 - Verify that 'puppet certregen --help' prints help text" do
+      # NOTE: `--help` only works on puppet version 4+
+      if version_is_less( '3.9.9', on(default, puppet('--version')).stdout)
+        describe command("puppet certregen --help") do
+          its(:stdout) { should match( /.*USAGE: puppet certregen <action>.*/ ) }
+          its(:stdout) { should match( /.*See 'puppet man certregen' or 'man puppet-certregen' for full help.*/ ) }
+        end
+      end
+    end
+    describe "C99812 - Verify that 'puppet help certregen' prints help text" do
+        describe command("puppet help certregen") do
+          its(:stdout) { should match( /.*USAGE: puppet certregen <action>.*/ ) }
+          its(:stdout) { should match( /.*See 'puppet man certregen' or 'man puppet-certregen' for full help.*/ ) }
+        end
+    end
+    describe "C99813 - Verify that 'puppet help certregen healthcheck' prints help text for healthcheck subcommand" do
+        describe command("puppet help certregen healthcheck") do
+          its(:stdout) { should match( /.*USAGE: puppet certregen healthcheck .*/ ) }
+          its(:stdout) { should match( /.*See 'puppet man certregen' or 'man puppet-certregen' for full help.*/ ) }
+        end
+    end
+    describe "C99814 - Verify that 'puppet help certregen ca' prints help text for ca subcommand" do
+        describe command("puppet help certregen ca") do
+          its(:stdout) { should match( /.*USAGE: puppet certregen ca .*/ ) }
+          its(:stdout) { should match( /.*See 'puppet man certregen' or 'man puppet-certregen' for full help.*/ ) }
+        end
     end
   end
-end
-describe "C99812 - Verify that 'puppet help certregen' prints help text" do
-    describe command("puppet help certregen") do
-      its(:stdout) { should match( /.*USAGE: puppet certregen <action>.*/ ) }
-      its(:stdout) { should match( /.*See 'puppet man certregen' or 'man puppet-certregen' for full help.*/ ) }
-    end
-end
-describe "C99813 - Verify that 'puppet help certregen healthcheck' prints help text for healthcheck subcommand" do
-    describe command("puppet help certregen healthcheck") do
-      its(:stdout) { should match( /.*USAGE: puppet certregen healthcheck .*/ ) }
-      its(:stdout) { should match( /.*See 'puppet man certregen' or 'man puppet-certregen' for full help.*/ ) }
-    end
-end
-describe "C99814 - Verify that 'puppet help certregen ca' prints help text for ca subcommand" do
-    describe command("puppet help certregen ca") do
-      its(:stdout) { should match( /.*USAGE: puppet certregen ca .*/ ) }
-      its(:stdout) { should match( /.*See 'puppet man certregen' or 'man puppet-certregen' for full help.*/ ) }
-    end
 end
